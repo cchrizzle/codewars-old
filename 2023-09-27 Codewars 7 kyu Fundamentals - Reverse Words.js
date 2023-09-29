507ea254bcf88f96a8e8e408ad1adfb569dfcd45
cchrizzle <64837366+cchrizzle@users.noreply.github.com>
2023-09-29 16:03:47 -0700

    Add files via upload

---
+++ 2023-09-27 Codewars 7 kyu Fundamentals - Reverse Words.js
@@ -0,0 +1,14 @@
+// 9/27/23 Codewars 7 kyu - Reverse Words
+
+/*
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+Examples
+
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+*/
+
+
+function reverseWords(str) {
+    return str.split('').reverse().join('').split(' ').reverse().join(' ');
+}
