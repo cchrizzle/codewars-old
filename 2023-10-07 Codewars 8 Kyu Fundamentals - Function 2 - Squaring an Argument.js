84999cd7be62f589503ab7763bfa0eedf3fb2c46
cchrizzle <64837366+cchrizzle@users.noreply.github.com>
2023-10-07 00:21:58 -0700

    Add files via upload

---
+++ 2023-10-07 Codewars 8 Kyu Fundamentals - Function 2 - Squaring an Argument.js
@@ -0,0 +1,7 @@
+// 10/07/23 Codewars 8 Kyu Fundamentals - Function 2 - Squaring an Argument
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+function square(arg) {
+    return arg ** 2;
+}
