fc29820736d8f8a69c563069efa19b37b43d8831
cchrizzle <64837366+cchrizzle@users.noreply.github.com>
2023-10-19 17:52:16 -0700

    Add files via upload

---
+++ 2023-10-19 Codewars 8 Kyu Fundamentals - Grasshopper - Debug sayHello.js
@@ -0,0 +1,16 @@
+// 10/19/23 Codewars Thursday 8 Kyu Fundamentals - Grasshopper - Debug sayHello
+
+// https://www.codewars.com/kata/5625618b1fe21ab49f00001f/javascript
+
+/*
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+*/
+
+// Code
+function sayHello(name) {
+    return `Hello, ${name}`;
+}
